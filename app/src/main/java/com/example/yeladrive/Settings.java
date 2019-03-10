d0fc5f7df5f5032860e29d85adc83be1ac635963
libbyellison <libbyellison@gmail.com>
2019-03-10 22:28:58 +0100

    This is the basic navigation bar-- still working on customizing for our app

---
+++ app/src/main/java/com/example/yeladrive/Settings.java
@@ -0,0 +1,27 @@
+package com.example.yeladrive;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Button switchToSettings = findViewById(R.id.button_to_main_screen);
+
+        switchToSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+}

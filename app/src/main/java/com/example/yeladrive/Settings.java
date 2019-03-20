f77da2ffb8f651f7e92b2e50c778d8a285b2828f
libbyellison <libbyellison@gmail.com>
2019-03-20 15:05:08 +0100

    This is with the UI of the settings page. Just preliminary so Boris can use for backend

--- app/src/main/java/com/example/yeladrive/Settings.java
+++ app/src/main/java/com/example/yeladrive/Settings.java
@@ -6,22 +6,4 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Settings extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        Button switchToSettings = findViewById(R.id.button_to_main_screen);
-
-        switchToSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-
-            }
-        });
-    }
-
-}

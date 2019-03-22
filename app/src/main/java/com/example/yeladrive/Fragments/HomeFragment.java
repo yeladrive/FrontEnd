e2454133d9cc667905b2340ca2569daa2034ad80
margalitnoa <noma5727@colorado.edu>
2019-03-22 15:23:20 +0100

    worked on home page, cannot see the recycler view displayed- might be an issue with fragments, added files

--- app/src/main/java/com/example/yeladrive/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/yeladrive/Fragments/HomeFragment.java
@@ -1,21 +1,89 @@
+//https://www.youtube.com/watch?v=kyGVgrLG3KU help from this tutorial
+
 package com.example.yeladrive.Fragments;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.yeladrive.R;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
 
+    private static final String TAG = "Firelog";
+    private RecyclerView mMainList;
+    private FirebaseFirestore mFirestore;
+    private UpcomingRidesAdapter upcomingRidesAdapter;
+    private List<UpcomingRides> upcomingRidesList;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        upcomingRidesList = new ArrayList<>();
+        upcomingRidesAdapter = new UpcomingRidesAdapter(upcomingRidesList);
+
+        mMainList = (RecyclerView) view.findViewById(R.id.main_list);
+
+        mMainList.setHasFixedSize(true);
+        mMainList.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        mMainList.setAdapter(upcomingRidesAdapter);
+
+
+
+
+        mFirestore = FirebaseFirestore.getInstance();
+
+        mFirestore.collection("Upcoming_Rides").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@javax.annotation.Nullable QuerySnapshot queryDocumentSnapshots, @javax.annotation.Nullable FirebaseFirestoreException e) {
+                if(e!=null){
+                    Log.d(TAG, "Error : " + e.getMessage());
+
+                }
+                for(DocumentChange doc: queryDocumentSnapshots.getDocumentChanges()){
+
+                    if(doc.getType() == DocumentChange.Type.ADDED){
+
+                        UpcomingRides upcomingRides = doc.getDocument().toObject(UpcomingRides.class);
+                        upcomingRidesList.add(upcomingRides);
+                        upcomingRidesAdapter.notifyDataSetChanged();
+
+                    }
+
+                    /*String kid_name = doc.getString("name");
+                    String pick_up_loc = doc.getString("pick_up_loc");
+                    Log.d(TAG, "Pick up : " + kid_name);*/
+
+
+                }
+
+            }
+        });
+
+
+
+
+        return view;
     }
 }
+
+

1de01898ec509297b7b248be994138b3ed4a2d2e
borisboeri <boris.boeri@gmail.com>
2019-03-24 21:50:33 +0100

    Home Fragment ready to receive UpComingRides

--- app/src/main/java/com/example/yeladrive/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/yeladrive/Fragments/HomeFragment.java
@@ -1,21 +1,108 @@
+//https://www.youtube.com/watch?v=kyGVgrLG3KU help from this tutorial
+
 package com.example.yeladrive.Fragments;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.yeladrive.R;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
 
+    private static final String TAG = "Firelog";
+    private RecyclerView mMainList;
+    private FirebaseFirestore mFirestore;
+    private UpComingRidesAdapter upcomingRidesAdapter;
+    private List<UpcomingRides> upcomingRidesList;
+    private TextView title;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+
+    @Override
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        title = (TextView) view.findViewById(R.id.textView2);
+        upcomingRidesList = new ArrayList<>();
+        //upcomingRidesList.add(new UpcomingRides("Ava", "school"));
+
+        for (UpcomingRides s : upcomingRidesList){
+            Log.d("My array list content: ", s.toString());
+        }
+
+        mMainList = (RecyclerView) view.findViewById(R.id.main_list);
+
+        mMainList.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        mMainList.setHasFixedSize(true);
+        // initializeData();
+
+
+        // upcomingRidesAdapter.notifyDataSetChanged();
+
+        mFirestore = FirebaseFirestore.getInstance();
+
+
+        upcomingRidesAdapter = new UpComingRidesAdapter(upcomingRidesList);
+        mMainList.setAdapter(upcomingRidesAdapter);
+
+        mFirestore.collection("Upcoming_Rides").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@javax.annotation.Nullable QuerySnapshot queryDocumentSnapshots, @javax.annotation.Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.d(TAG, "Error : " + e.getMessage());
+
+                }
+                for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
+
+                    if (doc.getType() == DocumentChange.Type.ADDED) {
+
+                        UpcomingRides upcomingRides = doc.getDocument().toObject(UpcomingRides.class);
+                        upcomingRidesList.add(upcomingRides);
+                        upcomingRidesAdapter.notifyDataSetChanged();
+
+                    }
+
+                    /*String kid_name = doc.getString("name");
+                    String pick_up_loc = doc.getString("pick_up_loc");
+                    Log.d(TAG, "Pick up : " + kid_name);*/
+                }
+            }
+        });
+
+
+
+    }
+
+
+
+    private void initializeData(){
+        upcomingRidesList = new ArrayList<>();
+        upcomingRidesList.add(new UpcomingRides("Ava", "school"));
+
     }
 }
+
+
---
+++ app/src/main/java/com/example/yeladrive/Fragments/UpcomingRides.java
@@ -0,0 +1,35 @@
+package com.example.yeladrive.Fragments;
+
+public class UpcomingRides {
+    String kid_name, pick_up_loc;
+
+    public UpcomingRides(){
+
+    }
+
+    public UpcomingRides(String kid_name, String pick_up_loc){
+        this.kid_name = kid_name;
+        this.pick_up_loc = pick_up_loc;
+
+    }
+    public String getKid_name() {
+        return kid_name;
+    }
+
+    public void setKid_name(String kid_name) {
+        this.kid_name = kid_name;
+    }
+
+    public String getPick_up_loc() {
+        return pick_up_loc;
+    }
+
+    public void setPick_up_loc(String pick_up_loc) {
+        this.pick_up_loc = pick_up_loc;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}

daf71ab182029494e9e2f4b76c536615c18c65e2
libbyellison <libbyellison@gmail.com>
2019-04-03 16:29:23 +0200

    creates kid class

--- app/src/main/java/com/example/yeladrive/KidActivity.java
+++ app/src/main/java/com/example/yeladrive/KidActivity.java
@@ -2,10 +2,12 @@ package com.example.yeladrive;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -24,22 +26,31 @@ public class KidActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 User user = new User(FirebaseAuth.getInstance());
+                if (name_editText.getText().length() != 0 && age_editText.getText().length() != 0 ){
+
+
+                    String name = name_editText.getText().toString();
+                    Log.d("Name", name);
+
+                    Integer age = (Integer.parseInt(age_editText.getText().toString()));
+
+                    Log.d("Age", age.toString());
+
+                    Kid newKid = new Kid(name, age);
+
+
+                    user.addNewKid(newKid);
+                    Log.i("New kid added: ", user.getListOfKids().toString());
+
+                    finish();
+
+                } else {
+                    Toast.makeText(KidActivity.this, "please enter all fields", Toast.LENGTH_SHORT).show();
+                }
+
+
                 //Log.i("Kid list emptyt:", user.getListOfKids().toString());
 
-                String name = name_editText.getText().toString();
-                Log.d("Name", name);
-
-                Integer age = (Integer.parseInt(age_editText.getText().toString()));
-
-                Log.d("Age", age.toString());
-
-                Kid newKid = new Kid(name, age);
-
-
-                user.addNewKid(newKid);
-                Log.i("New kid added: ", user.getListOfKids().toString());
-
-                finish();
 
             }
         });
